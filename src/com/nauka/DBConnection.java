f3e61fef54d3ff4055f017430d8b4c3e023e03b5
Andrzej <annddrzej@gmail.com>
2022-03-12 15:59:29 +0100

    Add checking if table company exists

--- src/com/nauka/DBConnection.java
+++ src/com/nauka/DBConnection.java
@@ -1,34 +1,55 @@
 package com.nauka;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.Statement;
+import java.sql.*;
 
 public class DBConnection {
 
     private static final String JDBC_DRIVER = "org.h2.Driver";
     private static final String DB_URL = "jdbc:h2:./db/";
     private static final String USER = "u";
-    private static final String PASS = "";
+    private static final String PASS = "p";
 
     public DBConnection(String[] args) {
         String dbName = getDbNameFromCmdLine(args);
         String fullUrl = DB_URL + dbName;
+        String tableName = "company";
 
         try {
             Class.forName(JDBC_DRIVER);
-            try (Connection connection = DriverManager.getConnection(fullUrl, USER, PASS);
+            try (Connection connection = DriverManager.getConnection(fullUrl);
                  Statement statement = connection.createStatement()) {
                 connection.setAutoCommit(true);
-                String sql = "CREATE TABLE COMPANY " +
-                        "(ID INTEGER not NULL, " +
-                        "NAME VARCHAR(255), " +
-                        "PRIMARY KEY (id))";
-                statement.executeUpdate(sql);
+
+                if (!tableExists(connection, tableName)) {
+                    String sql = "CREATE TABLE " + tableName +
+                            "(id INTEGER NOT NULL, " +
+                            "name VARCHAR(255), " +
+                            "PRIMARY KEY (id))";
+                    statement.executeUpdate(sql);
+                }
+
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+    }
+
+    private boolean tableExists(Connection connection, String tableName) throws SQLException {
+        tableName = tableName.toUpperCase();
+        DatabaseMetaData databaseMetaData = connection.getMetaData();
+
+        try (ResultSet resultSet = databaseMetaData.getTables(null, null, null, new String[]{"TABLE"})) {
+            while (resultSet.next()) {
+                if (resultSet.getString("TABLE_NAME").equals(tableName)) {
+                    return true;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return false;
 
     }
 
